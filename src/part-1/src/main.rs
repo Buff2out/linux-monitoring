4a1a7f6a097b10d965158d7ba460203f768ab758
rorikstr <slava.070299@gmail.com>
2025-04-30 17:19:04 +0300

    comment added

--- src/part-1/src/main.rs
+++ src/part-1/src/main.rs
@@ -3,6 +3,10 @@ use std::error::Error;
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = std::env::args().collect();
     // Проверка количества аргументов сразу
+// Так же можно: 
+//      let param = args.get(1).ok_or("Ошибка: требуется 1 параметр")?;
+// Но тогда не будет учтено когда введено больше 2 аргументов.
+// Поэтому.
     if args.len() != 2 {
         return Err(format!(
             "Ошибка: требуется 1 параметр, получено {}", 
