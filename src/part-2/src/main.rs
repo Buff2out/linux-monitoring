aea97ca47704268620782712e23266f810d5b78d
rorikstr <slava.070299@gmail.com>
2025-05-01 23:37:55 +0300

    first steps to using docs.rs deeper, without ai

--- src/part-2/src/main.rs
+++ src/part-2/src/main.rs
@@ -1,3 +1,15 @@
+// use std::alloc::System;
+use sysinfo::System;
+
+struct SystemInfo;
+
+impl SystemInfo {
+    fn hostname() {
+        println!("Hostname: {:?}", System::host_name())
+    }
+}
+
 fn main() {
     println!("linux-monitoring-2");
+    SystemInfo::hostname();
 }
