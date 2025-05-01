958b9721dc9572ad3ea6686c879344db1fa99689
rorikstr <slava.070299@gmail.com>
2025-05-01 19:30:05 +0300

    removed unnecessary

--- src/part-2/src/disk_info.sh
+++ src/part-2/src/disk_info.sh
@@ -5,11 +5,6 @@ ROOT_TOTAL_B=$(df -B1 / | awk 'NR==2 {print $2}')
 ROOT_USED_B=$(df -B1 / | awk 'NR==2 {print $3}')
 ROOT_FREE_B=$(df -B1 / | awk 'NR==2 {print $4}')
 
-# Конвертация в MB с точностью до 2 знаков
-# SPACE_ROOT=$(echo "scale=2; $ROOT_TOTAL_B / 1048576" | bc -l | xargs printf "%.2f")
-# SPACE_ROOT_USED=$(echo "scale=2; $ROOT_USED_B / 1048576" | bc -l | xargs printf "%.2f")
-# SPACE_ROOT_FREE=$(echo "scale=2; $ROOT_FREE_B / 1048576" | bc -l | xargs printf "%.2f")
-
 SPACE_ROOT=$(df / | grep dev | awk '{printf "%.2f MB", $2/(1024.)}')
 SPACE_ROOT_USED=$(df / | grep dev | awk '{printf "%.2f MB", $3/(1024.)}')
 SPACE_ROOT_FREE=$(df / | grep dev | awk '{printf "%.2f MB", $4/(1024.)}')
